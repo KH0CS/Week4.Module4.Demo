29e5bd133550e8e635d4cc8752db9dbab26a3604
KH0CS <kameronrodriguez@my.whccd.edu>
2025-11-05 19:42:50 -0800

    I added tge second par, the for loop.

--- wk4_lecture.js
+++ wk4_lecture.js
@@ -2,3 +2,11 @@ let myNumbers = [2, 3, 4, 5, 6, 7];
 console.log(myNumbers[5]);
 
 //I had to use [0] to get the first number "2", I used [5] to get the last number "7".
+function number1(array){
+for(let i = 0; i < array.length; i++){
+    console.log(number1[i]);
+    console.log("myNumbers[]")
+}
+}
+
+console.log(myNumbers);
